5130b9f0ea641613903593ddd96417e2aa8debc6
emmanuel lopez <emmanuel.lopez.g@icloud.com>
2021-06-27 12:50:24 -0500

    adding collectionView and assets

--- FastMenu/ViewController.swift
+++ FastMenu/ViewController.swift
@@ -12,13 +12,27 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     //system fucntions
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        <#code#>
+        return 5
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FoodCell", for: indexPath)
         return cell
     }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let screenSize = UIScreen.main.bounds
+        
+        return CGSize(width: screenSize.width * 0.94, height: 220)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+    }
+    
+    //functions
+    
+    var imageNames:[String] = ["1","2","3","4","5"]
     
 
     override func viewDidLoad() {
